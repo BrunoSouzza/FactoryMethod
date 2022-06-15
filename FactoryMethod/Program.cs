02c44f5ee8379243b6e6219ea69ff48c64e36dc2
BrunoSouzza <souza_0010@hotmail.com>
2022-06-15 01:29:31 -0300

    Calling in the Program class

--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,2 +1,35 @@
 ﻿
 
+using FactoryMethod.ConcreteCreator;
+using FactoryMethod.Creator;
+using FactoryMethod.Product;
+
+Console.WriteLine("Selecione seu cartão.");
+Console.WriteLine("1: Gold");
+Console.WriteLine("2: Black");
+Console.WriteLine("3: Platinum");
+var selected = Console.ReadLine().ToLower();
+
+Console.WriteLine("");
+
+
+CreditCardFactory creditCardFactory = null;
+
+switch (selected)
+{
+    case "1":
+        creditCardFactory = new GoldFactory();
+        break;
+    case "2":
+        creditCardFactory = new BlackFactory();
+        break;
+    case "3":
+        creditCardFactory = new PlatinumFactory();
+        break;
+    default:
+        break;
+}
+
+CreditCard creditCard = creditCardFactory.RequestCreditCard();
+
+Console.WriteLine($"Credit Card Type: {creditCard.GetType().Name} | Limit: {creditCard.Limit}");
