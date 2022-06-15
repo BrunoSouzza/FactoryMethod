f1bf7cd5234b24d2669aaa956c69d49b7b7cfea2
BrunoSouzza <souza_0010@hotmail.com>
2022-06-15 01:27:32 -0300

    Creating concret products

---
+++ FactoryMethod/ConcretProduct/CreditCardBlack.cs
@@ -0,0 +1,12 @@
+﻿using FactoryMethod.Product;
+
+namespace FactoryMethod.ConcretProduct
+{
+    public class CreditCardBlack : CreditCard
+    {
+        public CreditCardBlack()
+        {
+            Limit = 10000;
+        }
+    }
+}
---
+++ FactoryMethod/ConcretProduct/CreditCardGold.cs
@@ -0,0 +1,17 @@
+﻿using FactoryMethod.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.ConcretProduct
+{
+    public class CreditCardGold : CreditCard
+    {
+        public CreditCardGold()
+        {
+            Limit = 5000;
+        }
+    }
+}
